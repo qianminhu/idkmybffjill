6333d1db4b349d5a7c5eb3cd5df69e4ea75fd84b
FrancescoSTL <francescosapolizzi@gmail.com>
2015-07-25 11:09:32 -0700

    Emojis supported x8.

--- idkmybffjill/emoji_table.py
+++ idkmybffjill/emoji_table.py
@@ -2,7 +2,7 @@ from addict import Dict
 
 em = Dict()
 
-# -0 to 10
+# 0 to 10
 
 # optimism
 # love
@@ -12,32 +12,32 @@ em = Dict()
 # remorse
 # contempt
 # aggressivemness
-em.bowtie.op = 2
-em.bowtie.lo = 0
-em.bowtie.su = 0
-em.bowtie.aw = 0
-em.bowtie.di = -2
-em.bowtie.re = -2
-em.bowtie.co = -2
-em.bowtie.ag = 0
+em.bowtie.op = 7
+em.bowtie.lo = 5
+em.bowtie.su = 5
+em.bowtie.aw = 5
+em.bowtie.di = 3
+em.bowtie.re = 3
+em.bowtie.co = 3
+em.bowtie.ag = 3
 
-em.smile.op = 3
-em.smile.lo = 3
-em.smile.su = 1
-em.smile.aw = 1
-em.smile.di = -5
-em.smile.re = -4
-em.smile.co = -5
-em.smile.ag = -5
+em.smile.op = 8
+em.smile.lo = 8
+em.smile.su = 6
+em.smile.aw = 6
+em.smile.di = 0
+em.smile.re = 1
+em.smile.co = 0
+em.smile.ag = 0
 
-em.laughing.op = 4
-em.laughing.lo = 3
-em.laughing.su = 2
-em.laughing.aw = 3
-em.laughing.di = -5
-em.laughing.re = -5
-em.laughing.co = -5
-em.laughing.ag = -5
+em.laughing.op = 9
+em.laughing.lo = 8
+em.laughing.su = 7
+em.laughing.aw = 8
+em.laughing.di = 0
+em.laughing.re = 0
+em.laughing.co = 0
+em.laughing.ag = 0
 
 em.blush.op = 5
 em.blush.lo = 5
@@ -48,22 +48,23 @@ em.blush.re = -5
 em.blush.co = -5
 em.blush.ag = -5
 
-em.laughing.op
-em.laughing.op
-em.laughing.op
-em.laughing.op
-em.laughing.op
-em.laughing.op
-em.laughing.op
+em.laughing.op = 
+em.laughing.lo = 
+em.laughing.su = 
+em.laughing.aw = 
+em.laughing.di = 
+em.laughing.re = 
+em.laughing.co = 
+em.laughing.ag = 
 
-em.smiley.op
-em.smiley.op
-em.smiley.op
-em.smiley.op
-em.smiley.op
-em.smiley.op
-em.smiley.op
-em.smiley.op
+em.smiley.op = 
+em.smiley.lo = 
+em.smiley.su = 
+em.smiley.aw = 
+em.smiley.di = 
+em.smiley.re = 
+em.smiley.co = 
+em.smiley.ag = 
 
 em.relaxed.op
 em.relaxed.op
@@ -487,68 +488,292 @@ em.sleepy.op
 em.sleepy.op
 
 em.yum.op
+em.yum.op
+em.yum.op
+em.yum.op
+em.yum.op
+em.yum.op
+em.yum.op
+em.yum.op
 
+em.mask.op
+em.mask.op
+em.mask.op
+em.mask.op
+em.mask.op
+em.mask.op
+em.mask.op
 em.mask.op
 
 em.sunglasses.op
+em.sunglasses.op
+em.sunglasses.op
+em.sunglasses.op
+em.sunglasses.op
+em.sunglasses.op
+em.sunglasses.op
+em.sunglasses.op
 
+em.dizzy_face.op
+em.dizzy_face.op
+em.dizzy_face.op
+em.dizzy_face.op
+em.dizzy_face.op
+em.dizzy_face.op
+em.dizzy_face.op
 em.dizzy_face.op
 
 em.imp.op
+em.imp.op
+em.imp.op
+em.imp.op
+em.imp.op
+em.imp.op
+em.imp.op
+em.imp.op
 
+em.smiling_imp.op
+em.smiling_imp.op
+em.smiling_imp.op
+em.smiling_imp.op
+em.smiling_imp.op
+em.smiling_imp.op
+em.smiling_imp.op
 em.smiling_imp.op
 
 em.neutral_face.op
+em.neutral_face.op
+em.neutral_face.op
+em.neutral_face.op
+em.neutral_face.op
+em.neutral_face.op
+em.neutral_face.op
+em.neutral_face.op
 
+em.no_mouth.op
+em.no_mouth.op
+em.no_mouth.op
+em.no_mouth.op
+em.no_mouth.op
+em.no_mouth.op
+em.no_mouth.op
 em.no_mouth.op
 
 em.innocent.op
+em.innocent.op
+em.innocent.op
+em.innocent.op
+em.innocent.op
+em.innocent.op
+em.innocent.op
+em.innocent.op
 
+em.alien.op
+em.alien.op
+em.alien.op
+em.alien.op
+em.alien.op
+em.alien.op
+em.alien.op
 em.alien.op
 
 print("all colored hearts")
 em.heart.op
+em.heart.op
+em.heart.op
+em.heart.op
+em.heart.op
+em.heart.op
+em.heart.op
+em.heart.op
 
 em.broken_heart.op
+em.broken_heart.op
+em.broken_heart.op
+em.broken_heart.op
+em.broken_heart.op
+em.broken_heart.op
+em.broken_heart.op
+em.broken_heart.op
 
+em.cupid.op
+em.cupid.op
+em.cupid.op
+em.cupid.op
+em.cupid.op
+em.cupid.op
+em.cupid.op
 em.cupid.op
 
 print("all sparkles")
 em.sparkles.op
+em.sparkles.op
+em.sparkles.op
+em.sparkles.op
+em.sparkles.op
+em.sparkles.op
+em.sparkles.op
+em.sparkles.op
 
+em.fire.op
+em.fire.op
+em.fire.op
+em.fire.op
+em.fire.op
+em.fire.op
+em.fire.op
 em.fire.op
 
 print("all poop")
 em.poop.op
+em.poop.op
+em.poop.op
+em.poop.op
+em.poop.op
+em.poop.op
+em.poop.op
+em.poop.op
 
+em.thumbsup.op
+em.thumbsup.op
+em.thumbsup.op
+em.thumbsup.op
+em.thumbsup.op
+em.thumbsup.op
+em.thumbsup.op
 em.thumbsup.op
 
 em.thumbsdown.op
+em.thumbsdown.op
+em.thumbsdown.op
+em.thumbsdown.op
+em.thumbsdown.op
+em.thumbsdown.op
+em.thumbsdown.op
+em.thumbsdown.op
 
+em.raised_hands.op
+em.raised_hands.op
+em.raised_hands.op
+em.raised_hands.op
+em.raised_hands.op
+em.raised_hands.op
+em.raised_hands.op
 em.raised_hands.op
 
 em.pray.op
+em.pray.op
+em.pray.op
+em.pray.op
+em.pray.op
+em.pray.op
+em.pray.op
+em.pray.op
 
+em.clap.op
+em.clap.op
+em.clap.op
+em.clap.op
+em.clap.op
+em.clap.op
+em.clap.op
 em.clap.op
 
 em.muscle.up
+em.muscle.up
+em.muscle.up
+em.muscle.up
+em.muscle.up
+em.muscle.up
+em.muscle.up
+em.muscle.up
 
+em.fu.op
+em.fu.op
+em.fu.op
+em.fu.op
+em.fu.op
+em.fu.op
+em.fu.op
 em.fu.op
 
 em.couple.op
+em.couple.op
+em.couple.op
+em.couple.op
+em.couple.op
+em.couple.op
+em.couple.op
+em.couple.op
 
+em.family.op
+em.family.op
+em.family.op
+em.family.op
+em.family.op
+em.family.op
+em.family.op
 em.family.op
 
 em.kiss.op
+em.kiss.op
+em.kiss.op
+em.kiss.op
+em.kiss.op
+em.kiss.op
+em.kiss.op
+em.kiss.op
 
+em.tongue.op
+em.tongue.op
+em.tongue.op
+em.tongue.op
+em.tongue.op
+em.tongue.op
+em.tongue.op
 em.tongue.op
 
 em.lips.op
+em.lips.op
+em.lips.op
+em.lips.op
+em.lips.op
+em.lips.op
+em.lips.op
+em.lips.op
 
+em.no_good.op
+em.no_good.op
+em.no_good.op
+em.no_good.op
+em.no_good.op
+em.no_good.op
+em.no_good.op
 em.no_good.op
 
 em.dancer.op
+em.dancer.op
+em.dancer.op
+em.dancer.op
+em.dancer.op
+em.dancer.op
+em.dancer.op
+em.dancer.op
 
 em.angel.op
+em.angel.op
+em.angel.op
+em.angel.op
+em.angel.op
+em.angel.op
+em.angel.op
+em.angel.op
 
+em.smirk_cat.op
+em.smirk_cat.op
+em.smirk_cat.op
+em.smirk_cat.op
+em.smirk_cat.op
+em.smirk_cat.op
+em.smirk_cat.op
 em.smirk_cat.op
