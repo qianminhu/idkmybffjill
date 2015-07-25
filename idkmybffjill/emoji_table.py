faf02a9fbb608d21acec22bf1650e413a7f3b672
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-07-25 13:12:24 -0400

    added emoji data table

---
+++ idkmybffjill/emoji_table.py
@@ -0,0 +1,49 @@
+from addict import Dict
+
+em = Dict()
+
+# -5 to 5
+
+# optimism
+# love
+# submission
+# awe
+# disapproval
+# remorse
+# contempt
+# aggressivemness
+em.bowtie.op = 2
+em.bowtie.lo = 0
+em.bowtie.su = 0
+em.bowtie.aw = 0
+em.bowtie.di = -2
+em.bowtie.re = -2
+em.bowtie.co = -2
+em.bowtie.ag = 0
+
+em.smile.op = 3
+em.smile.lo = 3
+em.smile.su = 1
+em.smile.aw = 1
+em.smile.di = -5
+em.smile.re = -4
+em.smile.co = -5
+em.smile.ag = -5
+
+em.laughing.op = 4
+em.laughing.lo = 3
+em.laughing.su = 2
+em.laughing.aw = 3
+em.laughing.di = -5
+em.laughing.re = -5
+em.laughing.co = -5
+em.laughing.ag = -5
+
+em.blush.op = 5
+em.blush.lo = 5
+em.blush.su = 4
+em.blush.aw = 5
+em.blush.di = -5
+em.blush.re = -5
+em.blush.co = -5
+em.blush.ag = -5
